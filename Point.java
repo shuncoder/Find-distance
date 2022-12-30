7e4779642d9513f3b85a327041896b1066343ca5
shuncoder <vinhgatapkich@gmail.com>
2022-12-30 12:00:23 +0700

    find distance

---
+++ Point.java
@@ -0,0 +1,41 @@
+public class Point {
+	private double x;
+	private double y;
+
+	public Point() {
+	}
+
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public void setXY(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double distance(double x, double y) {
+		return Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+	}
+
+	public double distance(Point another) {
+		return distance(another.getX(), another.getY());
+	}
+}
