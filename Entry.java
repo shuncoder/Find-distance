e7a58ec4d1082d217801681b53ef94c4bcada0bd
Shuncoder <118190614+shuncoder@users.noreply.github.com>
2022-12-31 21:17:51 +0700

    Update Entry.java

--- Entry.java
+++ Entry.java
@@ -2,7 +2,8 @@ public class Entry {
 	public static void main(String[] args) {
 		Point p1 = new Point(4.2, 4.4);
 		Point p2 = new Point(6.7, 5.5);
-		System.out.println(p1.distance(p2));// p2 được lấy ra từ đối tượng distane có thuộc tính point
+		// p2 được lấy ra từ đối tượng distane có thuộc tính point
+		System.out.println(p1.distance(p2));
 		System.out.println(p1.distance(2.34, 7.8));
 	}
 }
